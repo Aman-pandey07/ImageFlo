96e0cf8285d925bc22beb7f4d5b86cb50231a452
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-09-07 23:09:47 +0530

    top app bar Scroll behaviour implemented

--- app/src/main/java/com/aman/imagevista/MainActivity.kt
+++ app/src/main/java/com/aman/imagevista/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.aman.imagevista
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aman.imagevista.presentation.home_screen.HomeScreen
@@ -17,13 +21,29 @@ import com.aman.imagevista.presentation.home_screen.HomeViewModel
 import com.aman.imagevista.presentation.theme.ImageVistaTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ImageVistaTheme {
+                val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 val viewModel = viewModel<HomeViewModel>()
-                HomeScreen(images =viewModel.images, onImageClick = {}, onSearchClick = {})
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehaviour.nestedScrollConnection)
+                ) {
+                    HomeScreen(
+                        scrollBehavior = scrollBehaviour,
+                        images =viewModel.images,
+                        onImageClick = {},
+                        onSearchClick = {}
+                    )
+                }
+
+
             }
         }
     }
--- app/src/main/java/com/aman/imagevista/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/aman/imagevista/presentation/home_screen/HomeScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,14 +18,17 @@ import com.aman.imagevista.presentation.component.ImageCard
 import com.aman.imagevista.presentation.component.ImageVistaTopAppBar
 import com.aman.imagevista.presentation.component.ImagesVerticalGrid
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
+    scrollBehavior: TopAppBarScrollBehavior,
     images:List<UnsplashImage>,
     onImageClick:(String)->Unit,
     onSearchClick:()->Unit,
 ) {
     Column {
         ImageVistaTopAppBar(
+            scrollBehavior = scrollBehavior,
             onSearchClick = onSearchClick
         )
         ImagesVerticalGrid(
